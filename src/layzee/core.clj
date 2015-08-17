ab80ac25c3d62511823daa402e5ee1b24d18fe40
Ben Biddington <ben.biddington@gmail.com>
2015-08-18 11:27:10 +1200

    [CLI] `lein run` prints a real search for <#lazyweb>

--- src/layzee/core.clj
+++ src/layzee/core.clj
@@ -1,7 +1,19 @@
 (ns layzee.core
   (:gen-class)
   (:require [clojure.test :refer :all]
-            [layzee.adapters.twitter :refer :all :as twitter]))
+            [layzee.adapters.twitter :refer :all :as twitter]
+            [layzee.adapters.settings :refer :all :as settings]))
+
+(defn- clean[text]
+  (.replace text "\n" ""))
+
+(defn- view[t]
+  (format "[%s] %s -- %s" (:created_at t) (-> t :text clean) (-> t :user :name)))
+
 
 (defn -main [& args]
-  (println "Hello, World!"))
+  (let [result (twitter/lazy-web settings/consumer-token)]
+    (println (format "Searched for <#lazyweb> and found <%s> results" (count result)))
+
+    (doseq [tweet result]
+      (println (view tweet)))))
