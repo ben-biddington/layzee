b8afef0a623b855b64fe0caf0b76bcc3f0fe2ffd
Ben Biddington <ben.biddington@gmail.com>
2015-08-17 21:22:29 +1200

    Getting closer

--- src/layzee/adapters/twitter.clj
+++ src/layzee/adapters/twitter.clj
@@ -1,18 +1,40 @@
 (ns layzee.adapters.twitter
-  (:require [clj-http.client :as http]))
+  (:require [clj-http.client :as http]
+            [clj-http.util :as util]))
 
 ;; https://dev.twitter.com/oauth/application-only
 ;; https://github.com/dakrone/clj-http
 ;; https://apps.twitter.com/app/8673064
 
-(defn- sign[consumer-token]
-  (let [key :key secret :secret] consumer-token))
+(defn- %[what] (util/url-encode what))
+(defn- %64[what] (util/base64-encode (util/utf8-bytes what)))
+
+(defn sign[consumer-token]
+  (let [{key :key secret :secret} consumer-token]
+    (%64 (str (% key) ":" (% secret))))) ;; -> https://dev.twitter.com/oauth/application-only
+
+(defn- headers[consumer-token]
+  {
+   "Authorization" (str "Basic " (sign consumer-token))
+   "Content-type" "application/x-www-form-urlencoded;charset=UTF-8"})
 
 (defn- bearer-token[consumer-token]
-  (sign consumer-token))
+  (println (headers consumer-token))
+  (let [reply (http/post
+               "https://api.twitter.com/oauth2/token"
+               {
+                :headers (headers consumer-token)
+                :body "grant_type=client_credentials"})]
+    (println reply)
+    ))
+
+(defn- env[name] (or (System/getenv name) "UNKNOWN")) 
 
 (defn lazy-web []
-  (println (bearer-token {:key (System/getenv "TWITTER_CONSUMER_KEY") :secret (System/getenv "TWITTER_CONSUMER_SECRET")}))
-  (let [reply (http/post (str "http://api.twitter.com/oauth2/token"))]
-    (println reply))
+  (println (str "KEY:" (env "TWITTER_CONSUMER_KEY")))
+  (println (str "SECRET:" (env "TWITTER_CONSUMER_SECRET")))
+  
+  (println (bearer-token {:key (env "TWITTER_CONSUMER_KEY") :secret (env "TWITTER_CONSUMER_SECRET")}))
+  ;;(let [reply ()]
+  ;;  (println reply))
   [0])
--- test/layzee/integration/twitter_examples.clj
+++ test/layzee/integration/twitter_examples.clj
@@ -8,7 +8,13 @@
        (let [result (twitter/lazy-web)]
          (< 0 (count result)) => true)))
 
+;;(facts
+;; (fact "Each tweet contains the tag #lazyweb"
+;;       (let [result (first (twitter/lazy-web))]
+;;         (.contains "lazyweb" (:tags result)) => true)))
+
 (facts
- (fact "Each tweet contains the tag #lazyweb"
-       (let [result (first (twitter/lazy-web))]
-          (.contains "lazyweb" (:tags result)) => true)))
+ (fact "Signing works like this"
+       ;; the example from -> https://dev.twitter.com/oauth/application-only
+       (let [result (twitter/sign {:key "xvz1evFS4wEEPTGEFPHBog" :secret "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg"})]
+         result => "eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw==")))
