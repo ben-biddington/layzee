70bc5147c6df788c702733e5d825c12597eb1720
Ben Biddington <ben.biddington@gmail.com>
2015-08-17 22:03:02 +1200

    Fail unless you supply credentials, see <https://apps.twitter.com/app/8673064/keys>

--- test/layzee/integration/twitter_examples.clj
+++ test/layzee/integration/twitter_examples.clj
@@ -3,14 +3,18 @@
   (:require [clojure.test :refer :all]
             [layzee.adapters.twitter :refer :all :as twitter]))
 
-(defn- env[name] (or (System/getenv name) "UNKNOWN"))
+(defn- fail[reason & args]
+  ((throw (Exception. (apply format reason args)))))
+  
+(defn- env[name] (or (System/getenv name) (fail "You need to supply the <%s> environment variable" name)))
+
 (def ^{:private true} consumer-token
      {:key (env "TWITTER_CONSUMER_KEY") :secret (env "TWITTER_CONSUMER_SECRET")})
 
 (facts
- (fact "Can read the timeline"
+ (fact "Can read the timeline (and the oage size is 15)"
        (let [result (twitter/lazy-web consumer-token)]
-         (< 0 (count result)) => true)))
+         (count result) => 15)))
 
 (facts
  (fact "Each tweet contains the tag #lazyweb"
