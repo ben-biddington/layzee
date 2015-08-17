04640fa8a7e70a9425f4267f819abf1b4ba22596
Ben Biddington <ben.biddington@gmail.com>
2015-08-17 21:40:43 +1200

    [=> GREEN] Can search for lazyweb

--- src/layzee/adapters/twitter.clj
+++ src/layzee/adapters/twitter.clj
@@ -1,6 +1,7 @@
 (ns layzee.adapters.twitter
   (:require [clj-http.client :as http]
-            [clj-http.util :as util]))
+            [clj-http.util :as util]
+            [clojure.data.json :as json]))
 
 ;; https://dev.twitter.com/oauth/application-only
 ;; https://github.com/dakrone/clj-http
@@ -9,7 +10,7 @@
 (defn- %[what] (util/url-encode what))
 (defn- %64[what] (util/base64-encode (util/utf8-bytes what)))
 
-(defn sign[consumer-token]
+(defn- sign[consumer-token]
   (let [{key :key secret :secret} consumer-token]
     (%64 (str (% key) ":" (% secret))))) ;; -> https://dev.twitter.com/oauth/application-only
 
@@ -18,23 +19,26 @@
    "Authorization" (str "Basic " (sign consumer-token))
    "Content-type" "application/x-www-form-urlencoded;charset=UTF-8"})
 
-(defn- bearer-token[consumer-token]
+(defn- bearer-auth[token]
+  {
+   "Authorization" (str "Bearer " token)
+   "Accept" "application/json"})
+
+(defn- bearer-token-for[consumer-token]
   (println (headers consumer-token))
   (let [reply (http/post
                "https://api.twitter.com/oauth2/token"
                {
                 :headers (headers consumer-token)
                 :body "grant_type=client_credentials"})]
-    (println reply)
-    ))
+    (:access_token (json/read-str (:body reply) :key-fn keyword))))
 
-(defn- env[name] (or (System/getenv name) "UNKNOWN")) 
+(defn- search[bearer-token what & opts]
+  (let [reply (http/get
+               "https://api.twitter.com//1.1/statuses/user_timeline.json?count=100&screen_name=twitterapi"
+               {:headers (bearer-auth bearer-token)})]
+    (json/read-str (:body reply) :key-fn keyword)))
 
-(defn lazy-web []
-  (println (str "KEY:" (env "TWITTER_CONSUMER_KEY")))
-  (println (str "SECRET:" (env "TWITTER_CONSUMER_SECRET")))
-  
-  (println (bearer-token {:key (env "TWITTER_CONSUMER_KEY") :secret (env "TWITTER_CONSUMER_SECRET")}))
-  ;;(let [reply ()]
-  ;;  (println reply))
-  [0])
+(defn lazy-web [consumer-token]
+  (let [token (bearer-token-for consumer-token)]
+    (search token "lazyweb")))
--- test/layzee/integration/twitter_examples.clj
+++ test/layzee/integration/twitter_examples.clj
@@ -3,18 +3,16 @@
   (:require [clojure.test :refer :all]
             [layzee.adapters.twitter :refer :all :as twitter]))
 
+(defn- env[name] (or (System/getenv name) "UNKNOWN"))
+(def ^{:private true} consumer-token
+     {:key (env "TWITTER_CONSUMER_KEY") :secret (env "TWITTER_CONSUMER_SECRET")})
+
 (facts
  (fact "Can read the timeline"
-       (let [result (twitter/lazy-web)]
+       (let [result (twitter/lazy-web consumer-token)]
          (< 0 (count result)) => true)))
 
 ;;(facts
 ;; (fact "Each tweet contains the tag #lazyweb"
 ;;       (let [result (first (twitter/lazy-web))]
 ;;         (.contains "lazyweb" (:tags result)) => true)))
-
-(facts
- (fact "Signing works like this"
-       ;; the example from -> https://dev.twitter.com/oauth/application-only
-       (let [result (twitter/sign {:key "xvz1evFS4wEEPTGEFPHBog" :secret "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg"})]
-         result => "eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw==")))
