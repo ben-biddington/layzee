36316f8c73f708c5c61c2699f2695a762affe678
Ben Biddington <ben.biddington@gmail.com>
2015-08-19 19:49:09 +1200

    You can use the file <.twitter> to store your credentials instead of env vars, see <.twitter.example>

--- src/layzee/adapters/settings.clj
+++ src/layzee/adapters/settings.clj
@@ -1,17 +1,29 @@
 (ns layzee.adapters.settings
-  (:gen-class)
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
 
 (defn- fail[reason & args]
   ((throw (Exception. (apply format reason args)))))
 
 (defn- env?[name] (not (nil? (System/getenv name))))
 
-(defn- env[name] (or (System/getenv name) (fail "You need to supply the <%s> environment variable" name)))
+(def settings-file-name ".twitter")
+(defn exists? [] (.exists (io/as-file settings-file-name)))
+
+(defn- from-disk []
+  (when (exists?)
+    (let [f (slurp settings-file-name)]
+      (json/read-str f :key-fn keyword))))
+
+(defn- env[name] (or (System/getenv name) )) ;; 
+
+(defn- from-env[] { :key (env "TWITTER_CONSUMER_KEY")  :secret (env "TWITTER_CONSUMER_SECRET")} )
 
 (def consumer-token
-     {
-      :key (env "TWITTER_CONSUMER_KEY")
-      :secret (env "TWITTER_CONSUMER_SECRET")})
+     (or
+      (from-disk)
+      (from-env)
+      (fail "You need to supply the <%s> environment variable, or the <%s> file" name settings-file-name)))
 
 (def log? (env? "LOG"))
--- test/layzee/integration/twitter_examples.clj
+++ test/layzee/integration/twitter_examples.clj
@@ -8,7 +8,13 @@
  (fact "Can read the timeline (and the page size is 15)"
        (let [result (twitter/lazy-web settings/consumer-token)]
          (count result) => 15)))
+
 (facts
  (fact "Each tweet contains the tag #lazyweb"
        (let [result (first (twitter/lazy-web settings/consumer-token))]
          (.contains (:text result) "#lazyweb") => true)))
+
+(facts
+ (fact "Each tweet contains its replies (?)"
+       (let [result (first (twitter/lazy-web settings/consumer-token))]
+         (println result)))
