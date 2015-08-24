aa57e109775a7b8f2150c01039b05c48aaf088df
Ben Biddington <ben.biddington@gmail.com>
2015-08-24 20:12:05 +1200

    [EXP] Added reference to bone (oauth for clojure) and started on working out how to sign requests

--- test/layzee/integration/twitter_streaming_examples.clj
+++ test/layzee/integration/twitter_streaming_examples.clj
@@ -2,10 +2,14 @@
   (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [layzee.adapters.twitter :refer :all :as twitter]
-            [layzee.adapters.settings :refer :all :as settings]))
+            [layzee.adapters.settings :refer :all :as settings]
+            [bone.signature-base-string :refer :all :as signature_base_string]))
 
 (facts
  (fact :wip "Can connect"
        (let [result (twitter/stream-connect settings/consumer-token)]
          (println result))))
+
+(facts
+ (fact "can make a signature base string"))
 
