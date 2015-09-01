02ea6e6c1e5b7c013d67cddae048f5d30e559927
Ben Biddington <ben.biddington@gmail.com>
2015-09-01 14:46:00 +1200

    Record user name too

--- src/layzee/use_cases/realtime_sample.clj
+++ src/layzee/use_cases/realtime_sample.clj
@@ -1,15 +1,17 @@
 (ns layzee.use-cases.realtime-sample
   (:require [layzee.adapters.journal :as journal]))
 
+(defn- clean[text] (.replace text "\n" ""))
+
 (defn- console[tweet]
-  (let [text (get tweet "text") created-at (get tweet "created_at")]
+  (let [text (clean (get tweet "text")) created-at (get tweet "created_at")]
     (when (not (nil? text))
       (println (format "[%s] -- %s" created-at text)))))
 
 (defn- record[tweet]
-  (let [text (get tweet "text") created-at (get tweet "created_at")]
+  (let [text (clean (get tweet "text")) created-at (get tweet "created_at") screen-name (get-in tweet ["user" "screen_name"])]
     (when (not (nil? text))
-      (journal/record (format "[%s] -- %s" created-at text)))))
+      (journal/record (format "[%s] -- @%s -- %s" created-at screen-name text)))))
 
 (defn- view[tweet]
   (console tweet)
