908b79dcf23a575ea120a75cc4f8a251b4c49a10
Ben Biddington <ben.biddington@gmail.com>
2015-09-02 20:17:45 +1200

    [FAILING] Can find replies like this (by screen scraping)

--- test/layzee/integration/finding_replies.clj
+++ test/layzee/integration/finding_replies.clj
@@ -5,14 +5,20 @@
             [layzee.adapters.settings :refer :all :as settings]
             [layzee.adapters.twitter.api :as api]
             [layzee.adapters.twitter.search :as search]
+            [layzee.adapters.twitter.replies :as replies]
             [layzee.adapters.logging :refer :all]))
 
 (facts ;; https://dev.twitter.com/rest/public/search; https://dev.twitter.com/rest/reference/get/search/tweets
-
+ 
  (fact "Though searching by tweet id returns nothing" ;; https://twitter.com/benbiddington/status/636787150009184256
        (count (search/by-keyword settings/oauth-credential "636787150009184256")) => 0)
  
- (fact "Searching for something common returns some reasults"
+ (fact "Searching for something common returns some results"
        (let [result (search/by-keyword settings/oauth-credential "twitter")]
          (empty? result) => false)))
 
+(facts
+ (fact "Can find replies like this"
+       (let [result (replies/to {:id "636787150009184256" :screen-name "benbiddington"})]
+         (empty? result) => false
+       )))
