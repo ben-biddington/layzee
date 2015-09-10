a5a984af95272c35cf04925cb51d049b17109428
Ben Biddington <ben.biddington@gmail.com>
2015-09-11 10:43:52 +1200

    Refactor: duplication

--- test/layzee/integration/storage/simple_db_examples.clj
+++ test/layzee/integration/storage/simple_db_examples.clj
@@ -6,23 +6,27 @@
             [cemerick.rummage.encoding :as enc]
             [layzee.adapters.settings :refer :all :as settings]))
 
-(defn- set[amazon-credential key value] ;; https://console.aws.amazon.com/iam/home?region=us-west-2#security_credential
-  (let [client (sdb/create-client (-> amazon-credential :access-key-id) (-> amazon-credential :secret-access-key))]
-    (let [config (assoc enc/keyword-strings :client client)]
-      (sdb/create-domain config "bjb-demo")
-      (sdb/put-attrs config "bjb-demo" {::sdb/id key :name "value" :key value}))))
+;; https://github.com/cemerick/rummage
+(defn- client-for[amazon-credential] (sdb/create-client (-> amazon-credential :access-key-id) (-> amazon-credential :secret-access-key)))
+(defn- config-for[amazon-credential] (assoc enc/keyword-strings :client (client-for amazon-credential)))
 
-(defn- get[amazon-credential key] 
-  (let [client (sdb/create-client (-> amazon-credential :access-key-id) (-> amazon-credential :secret-access-key))]
-    (let [config (assoc enc/keyword-strings :client client)]
-      (sdb/create-domain config "bjb-demo")
-      (-> (sdb/get-attrs config "bjb-demo" key) :key))))
-  
+(defn- set[amazon-credential domain key value] ;; https://console.aws.amazon.com/iam/home?region=us-west-2#security_credential
+  (let [config (config-for amazon-credential)]
+    (sdb/put-attrs config domain {::sdb/id key :name "value" :key value})))
+
+(defn- get[amazon-credential domain key] 
+  (let [config (config-for amazon-credential)]
+    (-> (sdb/get-attrs config domain key) :key)))
+
 (facts "Basic proof that we can connect"
-       (fact "can write an entry and read it back"
-             (set settings/amazon-credential "Have you ever tried to eat a clock?" "It's very time consuming.")
-             (get settings/amazon-credential "Have you ever tried to eat a clock?") => "It's very time consuming.")
+       (let [domain "test-layzee-web"]
+         (sdb/create-domain (config-for settings/amazon-credential) domain)
+         
+         (fact "can write an entry and read it back"
+               (set settings/amazon-credential domain "Have you ever tried to eat a clock?" "It's very time consuming.")
+               (get settings/amazon-credential domain "Have you ever tried to eat a clock?") => "It's very time consuming.")
 
-       (future-fact "Getting something that does not exist returns nil")
-       
-         )
+         (future-fact "Getting something that does not exist returns nil")
+         (future-fact "What about when domain does not exist?")
+         (future-fact "You can list and delete domains")
+         ))
