8c07f1f342336795ae9461d95bf7c96d613c5953
Ben Biddington <ben.biddington@gmail.com>
2015-09-11 11:26:50 +1200

    Refactor: introduced <layzee.adapters.amazon.simple-db>

---
+++ src/layzee/adapters/amazon/simple_db.clj
@@ -0,0 +1,20 @@
+(ns layzee.adapters.amazon.simple-db
+  (:refer-clojure :exclude [get set])
+  (:require [cemerick.rummage :as sdb]
+            [cemerick.rummage.encoding :as enc]))
+
+;; https://github.com/cemerick/rummage
+(defn- client-for[amazon-credential] (sdb/create-client (-> amazon-credential :access-key-id) (-> amazon-credential :secret-access-key)))
+(defn- config-for[amazon-credential] (assoc enc/keyword-strings :client (client-for amazon-credential)))
+
+(defn- ex[amazon-credential fn] (apply fn (config-for amazon-credential) []))
+
+(defn set[amazon-credential domain key value] ;; https://console.aws.amazon.com/iam/home?region=us-west-2#security_credential
+  (ex amazon-credential #(sdb/put-attrs % domain {::sdb/id key :name "value" :key value})))
+
+(defn get[amazon-credential domain key] 
+  (ex amazon-credential #(-> (sdb/get-attrs % domain key) :key)))
+
+(defn create-domain[amazon-credential name]
+  (sdb/create-domain (config-for amazon-credential) name))
+
--- test/layzee/integration/storage/simple_db_examples.clj
+++ test/layzee/integration/storage/simple_db_examples.clj
@@ -2,36 +2,23 @@
   (:use midje.sweet)
   (:refer-clojure :exclude [get set])
   (:require [clojure.test :refer :all]
-            [cemerick.rummage :as sdb]
-            [cemerick.rummage.encoding :as enc]
+            [layzee.adapters.amazon.simple-db :as simple-db]
             [layzee.adapters.settings :refer :all :as settings]))
-
-;; https://github.com/cemerick/rummage
-(defn- client-for[amazon-credential] (sdb/create-client (-> amazon-credential :access-key-id) (-> amazon-credential :secret-access-key)))
-(defn- config-for[amazon-credential] (assoc enc/keyword-strings :client (client-for amazon-credential)))
-
-(defn- ex[amazon-credential fn] (apply fn (config-for amazon-credential) []))
-
-(defn- set[amazon-credential domain key value] ;; https://console.aws.amazon.com/iam/home?region=us-west-2#security_credential
-  (ex amazon-credential #(sdb/put-attrs % domain {::sdb/id key :name "value" :key value})))
-
-(defn- get[amazon-credential domain key] 
-  (ex amazon-credential #(-> (sdb/get-attrs % domain key) :key)))
 
 (facts "Basic proof that we can connect"
        (let [domain "test-layzee-web"]
 
-         (sdb/create-domain (config-for settings/amazon-credential) domain)
+         (simple-db/create-domain settings/amazon-credential domain)
          
          (fact "can write an entry and read it back"
-               (set settings/amazon-credential domain "Have you ever tried to eat a clock?" "It's very time consuming.")
-               (get settings/amazon-credential domain "Have you ever tried to eat a clock?") => "It's very time consuming.")
+               (simple-db/set settings/amazon-credential domain "Have you ever tried to eat a clock?" "It's very time consuming.")
+               (simple-db/get settings/amazon-credential domain "Have you ever tried to eat a clock?") => "It's very time consuming.")
 
          (fact "Getting something that does not exist returns nil"
-               (get settings/amazon-credential domain "xxx-does-not-exist-xxx") => nil)
+               (simple-db/get settings/amazon-credential domain "xxx-does-not-exist-xxx") => nil)
          
          (fact "Adding to non-existent domain fails with error"
-               (set settings/amazon-credential "xxx-domain-does-not-exist-xxx" "I used to have a fear of hurdles" "But I got over it") => (throws Exception #"The specified domain does not exist"))
+               (simple-db/set settings/amazon-credential "xxx-domain-does-not-exist-xxx" "I used to have a fear of hurdles" "But I got over it") => (throws Exception #"The specified domain does not exist"))
          
          (future-fact "You can list and delete domains")
          
