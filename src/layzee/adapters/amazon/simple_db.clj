2fda3412ea99ab1815af0274e63d080c2f45f998
Ben Biddington <ben.biddington@gmail.com>
2015-09-11 11:34:38 +1200

    (Simple DB) Can list domains

--- src/layzee/adapters/amazon/simple_db.clj
+++ src/layzee/adapters/amazon/simple_db.clj
@@ -9,6 +9,8 @@
 
 (defn- ex[amazon-credential fn] (apply fn (config-for amazon-credential) []))
 
+(defn- as-admin[amazon-credential fn] (apply fn (client-for amazon-credential) []))
+
 (defn set[amazon-credential domain key value] ;; https://console.aws.amazon.com/iam/home?region=us-west-2#security_credential
   (ex amazon-credential #(sdb/put-attrs % domain {::sdb/id key :name "value" :key value})))
 
@@ -17,4 +19,7 @@
 
 (defn create-domain[amazon-credential name]
   (ex amazon-credential #(sdb/create-domain % name)))
+
+(defn list-domains[amazon-credential]
+  (as-admin amazon-credential #(sdb/list-domains %)))
 
--- test/layzee/integration/storage/simple_db_examples.clj
+++ test/layzee/integration/storage/simple_db_examples.clj
@@ -6,20 +6,21 @@
             [layzee.adapters.settings :refer :all :as settings]))
 
 (facts "Basic proof that we can connect"
-       (let [domain "test-layzee-web"]
+       (let [domain "test-layzee-web" amazon-credential settings/amazon-credential]
 
-         (simple-db/create-domain settings/amazon-credential domain)
+         (simple-db/create-domain amazon-credential domain)
          
          (fact "can write an entry and read it back"
-               (simple-db/set settings/amazon-credential domain "Have you ever tried to eat a clock?" "It's very time consuming.")
-               (simple-db/get settings/amazon-credential domain "Have you ever tried to eat a clock?") => "It's very time consuming.")
+               (simple-db/set amazon-credential domain "Have you ever tried to eat a clock?" "It's very time consuming.")
+               (simple-db/get amazon-credential domain "Have you ever tried to eat a clock?") => "It's very time consuming.")
 
          (fact "Getting something that does not exist returns nil"
-               (simple-db/get settings/amazon-credential domain "xxx-does-not-exist-xxx") => nil)
+               (simple-db/get amazon-credential domain "xxx-does-not-exist-xxx") => nil)
          
          (fact "Adding to non-existent domain fails with error"
-               (simple-db/set settings/amazon-credential "xxx-domain-does-not-exist-xxx" "I used to have a fear of hurdles" "But I got over it") => (throws Exception #"The specified domain does not exist"))
+               (simple-db/set amazon-credential "xxx-domain-does-not-exist-xxx" "I used to have a fear of hurdles" "But I got over it") => (throws Exception #"The specified domain does not exist"))
          
-         (future-fact "You can list and delete domains")
+         (fact "You can list domains"
+               (simple-db/list-domains amazon-credential) => (contains domain) )
          
          ))
