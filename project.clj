68aafc3e59554290cc8750f1f847ac86ea0c0c38
Ben Biddington <ben.biddington@gmail.com>
2015-09-10 21:51:18 +1200

    [CLJS] Added deps + imports

--- project.clj
+++ project.clj
@@ -22,7 +22,8 @@
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [environ "0.5.0"]
-                 [org.clojure/core.memoize "0.5.6"]]
+                 [org.clojure/core.memoize "0.5.6"]
+		 [org.clojure/clojurescript "1.7.48"]]
   :main ^:skip-aot layzee.core
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[midje "1.7.0" :exclusions [org.clojure/clojure]]]
