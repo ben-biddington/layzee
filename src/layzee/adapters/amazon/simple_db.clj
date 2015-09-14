08dfc94f13657253712dd14742ea469c97464d98
Ben Biddington <ben.biddington@gmail.com>
2015-09-14 16:26:53 +1200

    Moved retry

--- src/layzee/adapters/amazon/simple_db.clj
+++ src/layzee/adapters/amazon/simple_db.clj
@@ -10,22 +10,33 @@
 (defn- client-for[amazon-credential] (sdb/create-client (-> amazon-credential :access-key-id) (-> amazon-credential :secret-access-key)))
 (defn- config-for[amazon-credential] (assoc enc/keyword-strings :client (client-for amazon-credential)))
 
-(defn- ex[amazon-credential fn] (apply fn (config-for amazon-credential) []))
+(def ^{:private true} exponential-backoff-strategy
+  (again/max-duration
+    10000
+    (again/max-retries
+      10
+      (again/randomize-strategy
+        0.5
+        (again/multiplicative-strategy 500 1.5)))))
 
-(defn- as-admin[amazon-credential fn] (apply fn (client-for amazon-credential) []))
+(defn- ex[amazon-credential fn]
+  (again/with-retries exponential-backoff-strategy
+                      (apply fn (config-for amazon-credential) [])))
+
+(defn- as-admin[amazon-credential fn]
+  (again/with-retries exponential-backoff-strategy
+                      (apply fn (client-for amazon-credential) [])))
 
 (defn encode[text] (json/write-str text))
 (defn decode[text] (json/read-str text :key-fn keyword))
 
 (defn set[amazon-credential domain key value] ;; https://console.aws.amazon.com/iam/home?region=us-west-2#security_credential
-  (again/with-retries [100 1000 10000]
-                      (ex amazon-credential #(sdb/put-attrs % domain {::sdb/id key :name "value" :key (encode value)}))))
+  (ex amazon-credential #(sdb/put-attrs % domain {::sdb/id key :name "value" :key (encode value)})))
 
 (defn get[amazon-credential domain key] 
-   (again/with-retries [100 1000 10000]
-                       (if-let [result (ex amazon-credential #(-> (sdb/get-attrs % domain key) :key))]
-                         (decode result)
-                         nil)))
+  (if-let [result (ex amazon-credential #(-> (sdb/get-attrs % domain key) :key))]
+    (decode result)
+    nil))
 
 (defn create-domain[amazon-credential name]
   (ex amazon-credential #(sdb/create-domain % name)))
