82865cd7734b1666878525855085b56ce85dfbd2
Ben Biddington <ben.biddington@gmail.com>
2015-09-16 19:34:23 +1200

    Make sure database is set up before test run

--- test/layzee/integration/conversation_examples.clj
+++ test/layzee/integration/conversation_examples.clj
@@ -7,7 +7,8 @@
             [layzee.adapters.twitter.nice-twitter :as nice-api]
             [layzee.adapters.twitter.search :as search]
             [layzee.adapters.twitter.conversation :as conversation]
-            [layzee.adapters.logging :refer :all]))
+            [layzee.adapters.logging :refer :all]
+            [layzee.adapters.amazon.dynamo-db :as db]))
 
 (facts "Find entire conversations like this" ;; https://dev.twitter.com/rest/public/search; https://dev.twitter.com/rest/reference/get/search/tweets
        (let [result (conversation/for #(api/get-tweet settings/oauth-credential %) "636840679272873984")]
@@ -22,6 +23,8 @@
                )))
 
 (facts "Find conversations from either cache or api"
+       (db/new-table settings/amazon-credential "test-layzee-web")
+       
        (let [result (conversation/for (nice-api/get-tweet settings/amazon-credential "test-layzee-web" settings/oauth-credential) "636840679272873984")]
          
          (fact "it returns the tweet"
