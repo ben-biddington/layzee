5579f2fc612f3c83b4f11de68dfb9ab8df0c11ad
Ben Biddington <ben.biddington@xero.com>
2015-09-17 12:18:43 +1200

    Delete this custom task as it is not surfaced

--- src/leiningen/clear.clj
+++
@@ -1,7 +0,0 @@
-(ns leiningen.clear
-  "Clear the storage database of all tables"
-  (:require [layzee.adapters.amazon.dynamo-db :as db]
-            [layzee.adapters.settings :as settings]))
-
-(defn clear [] 
-  (db/delete-all-tables settings/amazon-credential))
