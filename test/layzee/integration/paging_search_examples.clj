8ad3950edb4d4719b210111df7b2bc5a35a58ae6
Ben Biddington <ben.biddington@gmail.com>
2015-09-22 13:59:19 +1200

    A bit about paging

--- test/layzee/integration/paging_search_examples.clj
+++ test/layzee/integration/paging_search_examples.clj
@@ -9,9 +9,19 @@
             [layzee.adapters.logging :refer :all]))
 
 (facts "How to collect search results in pages"
-       (fact "for example, searching for mentions in small pages"
-             (let [search-results (search/by-keyword settings/twitter-bearer-token "@iamkey" {:count 20 :page-size 5})]
-               (println search-results)
-               (count search-results) => 20)
-             )
-       )
+       (future-fact "for example, searching for mentions in small pages"
+             (let [search-results (search/by-keyword settings/twitter-bearer-token "@iamkey" {:limit 3 :page-size 1})]
+               ;;(println search-results)
+               (count search-results) => 20)))
+
+(defn- page
+  ":fn-producer -- the source, :fn-stop -- how we tell we're finished, :result -- collects the results"
+  ([fn-producer fn-stop result]
+     (if (apply fn-stop [result])
+       result
+         (recur fn-producer fn-stop (concat (apply fn-producer []) result)))))
+
+(facts "How to page arbitrary sequences"
+       (fact "for example, collect 20 results 5 at a time"
+             (let [result (page #(range 5) #(<= 20 (count %)) '())]
+               (count result) => 20)))
