d91dc491e7f27ec305afabde4588ca4d972b041f
Ben Biddington <ben.biddington@gmail.com>
2015-10-02 11:00:42 +1300

    Delete comment

--- src/layzee/paging.clj
+++ src/layzee/paging.clj
@@ -1,28 +1,14 @@
 (ns layzee.paging)
 
-;(defn page
-;  ":fn-producer -- the source
-;   :fn-stop     -- how we tell we're finished
-;   :result      -- collects the results"
-;  ([fn-producer fn-stop result]
-;     (if (apply fn-stop [result])
-;       result
-;       (recur fn-producer fn-stop (concat (apply fn-producer []) result)))))
-
 (defn- default-fn-args[result args] args)
-
-(defn- produce[fn args]
-  (apply fn
-         (if (nil? args)
-           []
-           [args])))
+(defn- produce[fn args] (apply fn (if (nil? args) [] [args])))
 
 (defn page
   ":fn-producer -- function that produces the results
    :fn-args     -- function that produces arguments for the producer
    :fn-stop     -- how we tell we're finished
    :result      -- collects the results
-   :args        -- [optional] arguments for :fn-producer. Defaults to nil(, and :fn-producer is invoked with zero arity)."
+   :args        -- [optional] arguments for :fn-producer. Defaults to nil (, and :fn-producer is invoked with zero arity)."
   ([fn-producer fn-stop result]
      (page fn-producer default-fn-args fn-stop result nil))
   ([fn-producer fn-args fn-stop result args]
